38f3731744afc02ad014b38d792ad30443795499
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-04-06 10:58:45 +0800

    chore: Added logic for changing modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "/src/styles/style.css";
 import "/src/styles/modernize.css";
 import "./styles/styleCompo/sideBar.css";
+import "./styles/styleCompo/dialogModals.css";
 import SideBar from "./compo/sideBar";
 import DialogModal from "./compo/dialogModals";
 
@@ -10,8 +11,17 @@ let side_bar = document.querySelector("#side__bar");
 let side_bar_lower = document.querySelector(".side__bar-lower");
 let add_project_btn = document.querySelector(".add__project-btn");
 let dialog_container = document.querySelector(".dialog__container");
+let dialog_form = document.querySelector("#dialog__form");
+let close_dialog_btn = document.querySelector(".close__dialog");
 
 side_bar.insertBefore(SideBar(), side_bar_lower);
+
 add_project_btn.addEventListener("click", () => {
-  dialog_container.appendChild(DialogModal("Add Project"))
+  dialog_form.replaceChildren();
+  dialog_form.appendChild(DialogModal("Add Project"));
+  dialog_container.classList.remove("hidden");
+});
+
+close_dialog_btn.addEventListener("click", () => {
+  dialog_container.classList.add("hidden");
 });
