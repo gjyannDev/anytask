1f9114b8b1175b6d44875f083f6c64c3c80bc632
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-04-12 23:07:55 +0800

    chore: refactor the getTaskById to not depend on getProjectById

--- src/compo/tasks.js
+++ src/compo/tasks.js
@@ -1,10 +1,10 @@
 import { getProject, storeProject } from "./project";
 
 export function createTask(projectId, task) {
-  const allProjects = getProject()
+  const allProjects = getProject();
 
-  const index = allProjects.findIndex(p => p.id === projectId);
-  
+  const index = allProjects.findIndex((p) => p.id === projectId);
+
   if (index === -1) {
     console.error("Project not found");
     return;
@@ -16,20 +16,22 @@ export function createTask(projectId, task) {
 }
 
 export function getTaskByProject(projectId) {
-  const allProjects = getProject()
+  const allProjects = getProject();
 
-  const index = allProjects.findIndex(p => p.id === projectId);
+  const index = allProjects.findIndex((p) => p.id === projectId);
 
   return allProjects[index].getTask();
 }
 
-export function getTaskById(projectId, taskId) {
-  return getTaskByProject(projectId).find((p) => p.id === taskId)
+export function getTaskById(taskId) {
+  return getProject()
+  .flatMap(project => project.tasks)
+  .find(task => task.id === taskId);
 }
 
 export function updateTask(updatedTask) {
-  const allProjects = getProject()
-  
+  const allProjects = getProject();
+
   allProjects.forEach((project) => {
     const taskIndex = project.tasks.findIndex((t) => t.id === updatedTask.id);
 
@@ -42,24 +44,30 @@ export function updateTask(updatedTask) {
 }
 
 export function deleteTask(taskId, projectId) {
-  const allProjects = getProject()
-  const index = allProjects.findIndex(p => p.id === projectId);
-  let filterTask = undefined
+  const allProjects = getProject();
+  const index = allProjects.findIndex((p) => p.id === projectId);
+  let filterTask = undefined;
 
   allProjects.forEach((project) => {
     filterTask = project.tasks.filter((el) => el.id !== taskId);
   });
 
-  allProjects[index].removeTask(filterTask)
+  allProjects[index].removeTask(filterTask);
 
-  storeProject(allProjects)
+  storeProject(allProjects);
 }
 
 export function filterTaskDataNotCompleted(tasks) {
-  return tasks.filter((task) => task.is_completed === false)
+  return tasks.filter((task) => task.is_completed === false);
 }
 
 export function filterTaskDataCompleted(tasks) {
-  return tasks.filter((task) => task.is_completed === true)
+  return tasks.filter((task) => task.is_completed === true);
 }
 
+export function getAllTask(allProjects) {
+  return allProjects.reduce((acc, curr) => {
+    acc.push(...curr.tasks)
+    return acc
+  }, [])
+}
